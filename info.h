7ad73b9e8657ad0d303092d30a93d3aa3266dbec
Christopher Villamarín <alexpila.314@gmail.com>
2026-02-15 17:38:53 -0500

    feat: update info.h to show accurated info with the help command

--- info.h
+++ info.h
@@ -6,46 +6,71 @@
 #include <string.h>
 #include "colores.h"
 
-void capacidadMaxima()
-{
-    printf("%sCapacidad máxima de conversión entre números de diferentes bases:%s\n", CCELESTE, NORMAL);
-    printf("\tRANGO DE NÚMEROS %s[ 2^63 , -2^63 ]%s\n", NMORADO,NORMAL);
-    printf("\tRANGO DE NÚMEROS %s[ 9223372036854775808 , -9223372036854775808 ]%s\n", NMORADO,NORMAL);
-    printf("EXCEPCIÓN: Rango de números romanos %s[3999999999, -3999999999]%s\n",NMORADO,NORMAL);
-}
-
 void printline()
 {
     printf("%s---------------------------------------------------------------------------%s\n", HCELESTE, NORMAL);
 }
 
+void capacidadMaxima()
+{
+    printf("%sCapacidad máxima de la calculadora:%s\n", CCELESTE, NORMAL);
+    printf("\tRANGO NUMÉRICO: %s[ 2^63 , -2^63 ]%s\n", NMORADO, NORMAL);
+    printf("\tRANGO ROMANO: %s[ 3 999 999 999 , -3 999 999 999 ]%s\n", NMORADO, NORMAL);
+    printf("\t(Límite de entrada por consola para romanos: %s399 999 999%s)\n", NAMARILLO, NORMAL);
+}
+
 void sistemasNumericos()
 {
-    printf("%sSistemas numéricos de esta aplicación:%s\n",CCELESTE,NORMAL);
-    printf("\t%sBIN%s\t-->\t%sb%s\"%s[0-1]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sTRN%s\t-->\t%st%s\"%s[0-2]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sCTN%s\t-->\t%sc%s\"%s[0-3]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sQUI%s\t-->\t%sq%s\"%s[0-4]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sSNR%s\t-->\t%sx%s\"%s[0-5]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sHEP%s\t-->\t%ss%s\"%s[0-6]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sOCT%s\t-->\t%so%s\"%s[0-7]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sNON%s\t-->\t%sn%s\"%s[0-8]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sDEC%s\t-->\t%s[0-9]+%s\n",NROJO,NORMAL,NVERDE,NORMAL);
-    printf("\t%sHEX%s\t-->\t%sh%s\"%s[0-9a-fA-F]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\t%sROM%s\t-->\t%sr%s\"%s[Ii]|[Ll]|[c-dC-Dm-yM-Y]+%s\"%s\n",NROJO,NORMAL,NAZUL,NAMARILLO,NVERDE,NAMARILLO,NORMAL);
-    printf("\tSe puede admitir tanto mayúsculas como minúsculas: b\"111\" = B\"111\"\n");
+    printf("%sSistemas numéricos soportados (Acotar entre comillas):%s\n", CCELESTE, NORMAL);
+    printf("\t%sBIN%s: b\"[0-1]\"   | %sTRN%s: t\"[0-2]\"   | %sCTN%s: c\"[0-3]\"\n", NROJO, NORMAL, NROJO, NORMAL, NROJO, NORMAL);
+    printf("\t%sQUI%s: q\"[0-4]\"   | %sSNR%s: x\"[0-5]\"   | %sHEP%s: s\"[0-6]\"\n", NROJO, NORMAL, NROJO, NORMAL, NROJO, NORMAL);
+    printf("\t%sOCT%s: o\"[0-7]\"   | %sNNR%s: n\"[0-8]\"   | %sDEC%s: [0-9]\n", NROJO, NORMAL, NROJO, NORMAL, NROJO, NORMAL);
+    printf("\t%sHEX%s: h\"[0-9a-f]\"| %sROM%s: r\"[i-y]\"\n", NROJO, NORMAL, NROJO, NORMAL);
+    printf("\t%sEjemplo:%s b\"111.11\", H\"FF\", r\"mmmCCXIV\"\n", NAMARILLO, NORMAL);
+}
+
+void funcionesMatematicas()
+{
+    printf("%sFunciones Matemáticas:%s\n", CCELESTE, NORMAL);
+    printf("\t%sTrigonométricas:%s sen, cos, tan, sec, csc, ctg (usar 'deg' para grados)\n", NVERDE, NORMAL);
+    printf("\t%sInversas:%s asen, acos, atan, atan2\n", NVERDE, NORMAL);
+    printf("\t%sHiperbólicas:%s senh, cosh, tanh, asenh, acosh, atanh\n", NVERDE, NORMAL);
+    printf("\t%sLog/Exp:%s log, log10, log2, exp\n", NVERDE, NORMAL);
+    printf("\t%sOtras:%s sqrt, cbrt, abs, gamma, hypot, !\n", NVERDE, NORMAL);
+}
+
+void operadoresYAsignaciones()
+{
+    printf("%sOperadores y Variables:%s\n", CCELESTE, NORMAL);
+    printf("\t%sBásicos:%s +, -, *, /, %%, ^, !, |, ()\n", NAZUL, NORMAL);
+    printf("\t%sAsignación:%s = , += , -= , *= , /=\n", NAZUL, NORMAL);
+    printf("\t%sInc/Dec:%s ++var, --var, var++, var--\n", NAZUL, NORMAL);
+    printf("\t%sVariables:%s x = 10, y = b\"1010\"\n", NAZUL, NORMAL);
+}
+
+void comandosEspeciales()
+{
+    printf("%sComandos y Conversiones:%s\n", CCELESTE, NORMAL);
+    printf("\t%sexit%s: Salir | %stable%s: Ver variables | %sprintl%s: Línea divisoria\n", NAMARILLO, NORMAL, NAMARILLO, NORMAL, NAMARILLO, NORMAL);
+    printf("\t%sCONVERT_ALL(n)%s: Muestra 'n' en todas las bases.\n", NAMARILLO, NORMAL);
+    printf("\t%sBIN(n), HEX(n), ROM(n), ...%s: Convierte 'n' a la base indicada.\n", NAMARILLO, NORMAL);
 }
 
 void mostrarInfo()
 {
     printline();
-    printf("%sFUNCIONALIDADES DE ESTA CALCULADORA:%s\n",CCELESTE,NORMAL);
-    printf("\t- Reconoce números en base 2, 3, 4, 5, 6, 7, 8, 9, 10, 16.\n");
-    printf("\t- Reconoce números romanos. (Con minúsculas o mayúsculas)\n");
-    printf("\t- Puedes guardar ese valor en una variable: resultado = 10 \n");
-    printline(); capacidadMaxima();
-    printline(); sistemasNumericos();
+    printf("\t\t%sAYUDA - RECONOCEDOR DE EXPRESIONES%s\n", SCELESTE, NORMAL);
+    printline();
+    capacidadMaxima();
+    printline();
+    sistemasNumericos();
+    printline();
+    funcionesMatematicas();
+    printline();
+    operadoresYAsignaciones();
+    printline();
+    comandosEspeciales();
     printline();
 }
 
-#endif 
+#endif
